653e50a8bcbfd5b129a2ebefc8b42209dd7df7a9
yolandapadilla1 <yipadilla@miners.utep.edu>
2025-04-06 22:56:55 -0600

    CODE 8

--- code_7.py
+++ code_7.py
@@ -4,5 +4,14 @@ import matplotlib.pyplot as plt
 #If any of this libraries is missing from your computer. Please install them using pip.
 
 filename = 'Flight_Delays_2018.csv'
+df = pd.read_csv(filename)
 
+df.boxplot(column="ARR_DELAY", by="CARRIER_DELAY")
+
+
+plt.title('Flight_Delays_2018.csv')
+plt.xlabel('PREDICTED DELAYS IN ARRIVALS')
+plt.ylabel('ARR_DELAY')
+plt.show()
 #ARR_DELAY is the column name that should be used as dependent variable (Y).
+
